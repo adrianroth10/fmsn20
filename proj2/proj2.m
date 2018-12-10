27da8066644f9cf3e026fa085127ca1ac45a46e1
Anna <anna.ibs.s@gmail.com>
2018-12-10 21:17:31 +0100

    plots

--- proj2/proj2.m
+++ proj2/proj2.m
@@ -17,14 +17,13 @@ I(Ivalid) = false;
 [C,G,G2] = matern_prec_matrices([u1(:) u2(:)]);
 %mean value-vector (might not need all)
 %and observation matrix for the grid
-Agrid = speye(prod(sz));
 %Agrid = 0*speye(prod(sz));
-% Bgrid = [ones(prod(sz),1) bei_elev(:)];
-% Bgrid = [ones(prod(sz),1) bei_grad(:)];
-% % Bgrid = [ones(prod(sz),1) bei_elev(:) bei_grad(:)];
-% Bgrid = [ones(prod(sz),1) bei_elev(:) bei_elev(:).^2];
-% Bgrid = [ones(prod(sz),1) bei_grad(:) bei_grad(:).^2];
-Bgrid = [ones(prod(sz),1) bei_elev(:) bei_elev(:).^2 bei_grad(:) bei_grad(:).^2];
+%Bgrid = [ones(prod(sz),1) bei_elev(:)];
+%Bgrid = [ones(prod(sz),1) bei_grad(:)];
+%Bgrid = [ones(prod(sz),1) bei_elev(:) bei_grad(:)];
+%Bgrid = [ones(prod(sz),1) bei_elev(:) bei_elev(:).^2];
+Bgrid = [ones(prod(sz),1) bei_grad(:) bei_grad(:).^2];
+%Bgrid = [ones(prod(sz),1) bei_elev(:) bei_elev(:).^2 bei_grad(:) bei_grad(:).^2];
 Nbeta = size(Bgrid,2);
 qbeta = 1e-6;
 %and observation matrix for the grid
@@ -39,48 +38,8 @@ G2 = G2(p,p);
 Agrid = Agrid(:,p);
 %create A tilde matrix
 Atilde = [Agrid Bgrid];
-%we need a global variable for x_mode to reuse
-%between optimisation calls
-global x_mode;
-x_mode = [];
-%subset Y and Atilde to observed points
-par = fminsearch( @(x) GMRF_negloglike(x, Y(I), Atilde(I,:), C, ...
-G, G2, qbeta, false), [0 0]);
-%conditional mean is given by the mode
-E_xy = x_mode;
-E_zy = exp(Atilde * E_xy);
 
-%reuse taylor expansion to compute posterior precision
-tau = exp(par(1));
-kappa2 = exp(par(2));
-gamma = 1;
-Qcar = tau*(kappa2*C + G);
-Qsar = tau*(kappa2^2*C + 2*kappa2*G + G2);
-Qosc = tau*(kappa2^2*C + 2*gamma*kappa2*G + G2);
-Nbeta = size(Bgrid,2);
-Qtilde = blkdiag(Qsar, qbeta*speye(Nbeta));
-
-[~, f, Q_xy] = GMRF_taylor(E_xy, Y(I), Atilde(I, :), Qtilde);
-
-%% Variance of beta
-e = [zeros(size(Q_xy,1)-size(Bgrid,2), size(Bgrid,2)); eye(size(Bgrid,2))];
-V_beta0 = e'*(Q_xy\e);
-beta = E_xy(end - length(V_beta0) + 1:end)
-beta_int_size = sqrt(diag(V_beta0)) * 1.96
-
-
-%1000 samples from the approximate posterior
-Rxy = chol(Q_xy);
-x_samp = bsxfun(@plus, E_xy, Rxy\randn(size(Rxy,1),1000));
-
-
-Vx  = 1/(size(x_samp,2)-length(V_beta0))*sum(x_samp(1:end-length(V_beta0),:).^2,2);
-Vzy = Vx + sum((Bgrid*V_beta0).*Bgrid,2);
-std = Vzy.^(1/2);
-
-rms_error = validations(Y(Ivalid), E_zy(Ivalid), Vzy(Ivalid));  % sqrt(mean(Vzy(Ivalid).*(E_zy(Ivalid)- Y(Ivalid)).^2)));
-
-type = 1; % car model
+type = 2; % car model
 proj2_run;
 
 %%
--- proj2/proj2_run.m
+++ proj2/proj2_run.m
@@ -44,9 +44,12 @@ end
 %1000 samples from the approximate posterior
 Rxy = chol(Q_xy);
 x_samp = bsxfun(@plus, x_mode, Rxy \ randn(size(Rxy,1), 1000));
-Vx  = 1 / (size(x_samp,2) - Nbeta - length(par_init)) * sum(x_samp(1:end-Nbeta,:).^2,2);
+meanx_samp = mean(x_samp(1:end-Nbeta,:));
+z_samp = Atilde*x_samp;
+Vzy = var(z_samp,0,2);
+Vx  = 1 / (size(x_samp,2) - 1) * sum(x_samp(1:end-Nbeta,:).^2,2);
 Vbeta = sum((Bgrid * V_beta0) .* Bgrid, 2);
-Vzy = Vx + Vbeta;
+% Vzy = Vx + Vbeta;
 Vout = exp(Vzy);
 
 rms_error = sqrt(mean((E_out(Ivalid) - Y(Ivalid)).^2));
@@ -56,46 +59,52 @@ fprintf(1, 'rms_error: %.4f\nweighted_rms_error: %.4e\nfval: %.4e\n', rms_error,
 
 %% Plotting
 % Complete
-figure()
+figure(1)
 imagesc(reshape(E_out, sz))
 title('Estimated counts')
 colorbar
-return
 % Mean
-figure('Position', [100, 100, 1000, 400]);
-sgtitle('Mean component')
+figure('Position', [100, 100, 1400, 400]);
+%sgtitle('Mean component')
 subplot(1,2,1)
 imagesc(reshape(E_Bbeta, sz))
-title('Estimated counts')
+title('Mean component')
 colorbar
 subplot(1,2,2)
 imagesc(reshape(sqrt(Vbeta),sz))
 title('Standard deviation')
 colorbar
 % Spatial
-figure('Position', [100, 100, 1000, 400]);
-sgtitle('Spatial smooth component')
+figure('Position', [100, 100, 1400, 400]);
+%sgtitle('Spatial smooth component')
 subplot(1,2,1)
 title('Spatial component')
 imagesc(reshape(E_xy, sz))
-title('Estimated counts')
+title('Spatial component')
 colorbar
 subplot(1,2,2)
 imagesc(reshape(sqrt(Vx),sz))
 title('Standard deviation')
 colorbar
 % log complete
-figure('Position', [100, 100, 1000, 400]);
-sgtitle('Complete log intensity')
+figure('Position', [100, 100, 1400, 400]);
+%sgtitle('Complete log intensity')
 title('Log intensity of complete component')
 subplot(1,2,1)
 imagesc(reshape(E_zy, sz))
-title('Estimated counts')
+title('Latent field')
 colorbar
 subplot(1,2,2)
 imagesc(reshape(sqrt(Vzy),sz))
 title('Standard deviation')
 colorbar
 % Plot variance to distance
-figure()
+figure(5)
 plot(E_zy, Vzy, '*')
+xlabel('E(z|y)');
+ylabel('V(z|y)');
+
+figure(6)
+plot(x_samp(end-Nbeta+1,:), meanx_samp, '*')
+xlabel('\beta_0');
+ylabel('$\bar{x}_{samp}$','Interpreter', 'latex');
