3ba94f36cdb865a56e7791923a3f5f58c075e871
Adrian Roth <adrianroth10@gmail.com>
2018-12-14 15:35:52 +0100

    working on proj3

--- proj3/proj3_kmeans.m
+++ proj3/proj3_kmeans.m
@@ -2,5 +2,5 @@
 
 figure();
 imagesc(reshape(cl, sz(1:2)));
-print(['proj3/output/kmeans_', num2str(i_component), '_', num2str(nc), '_', num2str(is_beta), '.eps'], '-deps');
+print(['proj3/output/kmeans_', num2str(i_component), '_', num2str(nc), '_', num2str(is_beta), '.png'], '-dpng');
 close;
