b0c22f5b112b0f752b9e52e1d11ad1513e1a5c21
Anna <anna.ibs.s@gmail.com>
2018-12-16 11:09:06 +0100

    repotr

--- proj3/proj3.m
+++ proj3/proj3.m
@@ -7,14 +7,13 @@ sz = size(img);
 beta = X\colstack(img)';
 beta = reshape(beta', sz(1), sz(2), []);
 
-n_classes = [4 5 6 7 8];
+n_classes = [4 5 6 7 ];
+which_components = { [1,2] [2,3] [2,3,4]};
+
 is_beta = true;
 data = beta;
-which_components = { [2] [2,3] [2,3,4]};
 proj3_pca
-
 
 is_beta = false;
 data = img;
-which_components = { [2], [2,3], [2,3 4]};
 proj3_pca
