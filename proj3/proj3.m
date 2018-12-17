16adfe6143cbdcb93a21d8d2655393ca84e63512
Adrian Roth <adrianroth10@gmail.com>
2018-12-17 13:29:03 +0100

    working on the plots

--- proj3/proj3.m
+++ proj3/proj3.m
@@ -11,12 +11,14 @@ beta = X\colstack(img)';
 beta = reshape(beta', sz(1), sz(2), []);
 
 
-n_classes = [4 5 6 7 ];
-which_components = { [1,2] [2,3] [2,3,4]};
+n_classes = [4, 5];
+% which_components = {[1,2] [2,3] [2,3,4]};
+which_components = {[1]};
 
 is_beta = true;
 data = beta;
 proj3_pca
+return
 
 is_beta = false;
 data = img;
--- proj3/proj3_mrf_neighbours.m
+++ proj3/proj3_mrf_neighbours.m
@@ -26,13 +26,15 @@ for iter = 1:iterations
         theta{k}.Sigma = Sigma;
     end
     [alpha(iter + 1, 2:end), beta(iter + 1), acc_tmp] = ...
-                  gibbs_alpha_beta(alpha(iter, 2:end)', beta(iter), zmat, neighbours, 10, 1e-2);
+                  gibbs_alpha_beta(alpha(iter, 2:end)', beta(iter), zmat, neighbours, 1e-1, 1e-2);
     acc = acc + acc_tmp;
     if iter > burn_in
         zsum = zsum + zmat;
     end
 end
-acc = acc / iterations
+acc = acc / iterations;
+fprintf(1, 'acceptance rate = %.4f\n', acc);
+
 [zest, xest] = max(zsum / iterations, [], 3);
 
 figure();
@@ -42,15 +44,21 @@ close;
 
 figure();
 plot(Plog);
+xlabel('Gibbs iteration')
+ylabel('pseudo log likelihood of field')
 print(['proj3/output/mrf_plog_', num2str(is_beta), '_', str_components, '_', num2str(nc), '_', num2str(i_neighbours), '.png'], '-dpng');
 close;
 
 figure();
 plot(alpha(1:iterations, :));
+xlabel('Gibbs iteration')
+ylabel('\alpha values')
 print(['proj3/output/mrf_alpha_', num2str(is_beta), '_', str_components, '_', num2str(nc), '_', num2str(i_neighbours), '.png'], '-dpng');
 close;
 
 figure();
 plot(beta(1:iterations));
+xlabel('Gibbs iteration')
+ylabel('\beta values')
 print(['proj3/output/mrf_beta_', num2str(is_beta), '_', str_components, '_', num2str(nc), '_', num2str(i_neighbours), '.png'], '-dpng');
 close;
